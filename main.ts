bef58e9469f83cfa7e5f23f0819f348c3d681ae8
WhyK <windchimeyk@gmail.com>
2024-05-06 01:16:17 +0900

    feat: add functions without version up

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 export function add(a: number, b: number): number {
   return a + b;
 }
+export function subtra(a: number, b: number): number {
+  return a - b;
+}
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
+  console.log("Add 3 - 2 =", add(3, 2));
 }
